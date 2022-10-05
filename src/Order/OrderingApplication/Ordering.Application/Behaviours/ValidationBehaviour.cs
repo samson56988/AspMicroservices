0f331d35e4190756f731722e9e87f29c22d20183
Samson Ajayi <Samson.Ajayi@sterling.ng>
2022-10-05 10:00:44 +0100

    new

---
+++ src/Order/OrderingApplication/Ordering.Application/Behaviours/ValidationBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Behaviours
+{
+    //public class ValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    //{
+    //    public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //}
+}
